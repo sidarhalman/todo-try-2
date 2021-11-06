d951d631d85b3d026e8c40a6eb11af7a889a5c4f
sidarhalman <sidarhalman93@gmail.com>
2021-11-06 20:13:36 +0100

    html and css copleted start js

---
+++ index.js
@@ -0,0 +1,10 @@
+const formContainer = document.getElementById("form-container");
+console.log(formContainer);
+const todoInput = document.getElementById("todo-input");
+console.log(todoInput);
+const todoButton = document.getElementById("todo-button");
+console.log(todoButton);
+const todoListContainer = document.getElementById("todo-list-container");
+console.log(todoListContainer);
+const emptyAlert = document.getElementById("empty-alert");
+console.log(emptyAlert)
