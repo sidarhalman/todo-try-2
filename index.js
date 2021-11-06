e5c3d53a4796f5c6fe6dd1a970fb16d6b3467b47
sidarhalman <sidarhalman93@gmail.com>
2021-11-06 23:55:24 +0100

    add todoAdd func

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const formContainer = document.getElementById("form-container");
-console.log(formContainer);
 const todoInput = document.getElementById("todo-input");
-console.log(todoInput);
 const todoButton = document.getElementById("todo-button");
-console.log(todoButton);
 const todoListContainer = document.getElementById("todo-list-container");
-console.log(todoListContainer);
 const emptyAlert = document.getElementById("empty-alert");
-console.log(emptyAlert)
+const todoList = document.getElementById("todo-list");
+const resultAlert  = document.getElementById("result-alert");
+console.log(resultAlert)
+//---------------------------------- 
+
+todoButton.addEventListener("click", addTodo);
+
+
+function addTodo(event)
+{ 
+
+    if(todoInput.value !== "")
+    {
+
+        //create li element
+        const todoText =document.createElement("span");
+        const deleteButton = document.createElement("span");
+        const listItem = document.createElement("li");
+    
+        todoText.textContent = todoInput.value;
+        listItem.style.borderBottom = "1px solid #ddd"
+        deleteButton.textContent = "X";
+        deleteButton.style.border = "1px solid black"
+        deleteButton.style.fontSize = "17px"
+        deleteButton.style.margin = "0 5px 0 0";
+        listItem.appendChild(todoText);
+        listItem.appendChild(deleteButton);
+        listItem.style.display = "flex";
+        listItem.style.justifyContent = "space-between";
+    
+        todoList.appendChild(listItem);
+        emptyAlert.remove();
+        todoInput.value = "";
+        resultAlert.textContent = "Great!! You added a todo!"
+        resultAlert.style.background = "#D1E7DD";
+        resultAlert.style.color = "#455132"
+        
+        
+    }
+    else
+    {
+        emptyAlert.textContent = "You wrote anything ://.."
+        emptyAlert.style.backgroundColor = "#F8D7DA";
+        emptyAlert.style.color = "#AF3129"
+    }
+}
