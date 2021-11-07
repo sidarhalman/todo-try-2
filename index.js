26ce198c82ef9d80dd71144a10a3aa262285c3af
sidarhalman <sidarhalman93@gmail.com>
2021-11-07 15:09:32 +0100

    add delete function

--- index.js
+++ index.js
@@ -13,39 +13,59 @@ todoButton.addEventListener("click", addTodo);
 
 function addTodo(event)
 { 
-
     if(todoInput.value !== "")
     {
-
         //create li element
         const todoText =document.createElement("span");
         const deleteButton = document.createElement("span");
         const listItem = document.createElement("li");
-    
+        
         todoText.textContent = todoInput.value;
         listItem.style.borderBottom = "1px solid #ddd"
         deleteButton.textContent = "X";
+        deleteButton.id = "delete-button";
         deleteButton.style.border = "1px solid black"
         deleteButton.style.fontSize = "17px"
         deleteButton.style.margin = "0 5px 0 0";
+        deleteButton.style.cursor = "pointer";
         listItem.appendChild(todoText);
         listItem.appendChild(deleteButton);
         listItem.style.display = "flex";
         listItem.style.justifyContent = "space-between";
-    
+        
         todoList.appendChild(listItem);
-        emptyAlert.remove();
+        emptyAlert.textContent ="";
         todoInput.value = "";
         resultAlert.textContent = "Great!! You added a todo!"
         resultAlert.style.background = "#D1E7DD";
         resultAlert.style.color = "#455132"
-        
-        
     }
     else
     {
-        emptyAlert.textContent = "You wrote anything ://.."
-        emptyAlert.style.backgroundColor = "#F8D7DA";
-        emptyAlert.style.color = "#AF3129"
+        resultAlert.textContent = "You wrote anything ://.."
+        resultAlert.style.backgroundColor = "#F8D7DA";
+        resultAlert.style.color = "#AF3129"
     }
 }
+
+todoList.addEventListener("click", deleteTodoFromUI);
+
+function deleteTodoFromUI(event)
+{
+    const trgt = event.target;
+    if(trgt.id !== "empty-alert" && trgt.id == "delete-button")
+    {
+        console.log("Delete Todo");
+        trgt.parentElement.remove();
+        resultAlert.textContent ="You deleted a Todo!"
+        if(todoList.children.length === 0)
+        {
+            emptyAlert.textContent = "Your list is empty";
+        }
+    }
+}
+
+
+
+
+
